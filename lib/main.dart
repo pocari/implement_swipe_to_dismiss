7f0d7c742f70e5d9099687dea0e5f6b17f29b0ff
pocari <caffelattenonsugar@gmail.com>
2019-12-07 14:26:32 +0900

    create a data source

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyPage extends StatelessWidget {
+  final items = List<String>.generate(20, (i) => "Item ${i + 1}");
   @override
   Widget build(BuildContext context) => Center(
-        child: Text('Hello, world'),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text('${items[index]}'),
+          ),
+        ),
       );
 }
